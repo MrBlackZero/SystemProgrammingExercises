57c41bc8c0fd66097b72c38bd408e133ebbf8eb9
Mohammad <63192240+MrBlackZero@users.noreply.github.com>
2023-03-21 17:34:31 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strsafe.h>
+#include"system_info.h"
+int main() {
+	printf_s("\tExercises system programming\t\n\n");
+	printf_s("programmer : MrBlackZero\n");
+	printf_s("--------------------------------------\n");
+	print_versionwindows();
+	print_computername();
+	print_sysinfo();
+	printf_s("--------------------------------------\n");
+	puts("Press any key to close this window...");
+	getchar();
+	return EXIT_SUCCESS;
+}
+
