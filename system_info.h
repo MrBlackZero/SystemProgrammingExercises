920cfe10ad4adc02ff1b7de7c2a65f5dbd2f7367
Mohammad <63192240+MrBlackZero@users.noreply.github.com>
2023-03-21 17:21:38 +0330

    Delete system_info.h

--- system_info.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-void print_sysinfo(); //signature get system inforamation -> proc and memory
-void print_computername(); // signature function get computer name target
-void print_versionwindows(); //signature function print windows version -> MajorVersion.MinorVersion.dwBuildNumber
-void print_ramsize();
