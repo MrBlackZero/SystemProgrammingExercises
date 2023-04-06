b702858fe81410e9b320846fc0c979e91193f35c
Mohammad <63192240+MrBlackZero@users.noreply.github.com>
2023-04-06 21:53:48 +0330

    Update system_info.h

--- system_info.h
+++ system_info.h
@@ -1,5 +1,5 @@
 #pragma once
 void print_sysinfo(); //signature get system inforamation -> proc and memory
 void print_computername(); // signature function get computer name target
-void print_versionwindows(); //signature function print windows version -> MajorVersion.MinorVersion.dwBuildNumber
+void print_versionwindows(); //signature function print windows version
 void print_ramsize();
